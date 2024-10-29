396d1580bec7894ee2f191e61a4f9e4928fba079
professionalowo <martin.brandl1307@gmail.com>
2024-10-29 21:38:34 +0100

    Added logegr provider

--- YoutubeDownloaderWpf/App.xaml.cs
+++ YoutubeDownloaderWpf/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
@@ -7,6 +8,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 using YoutubeDownloaderWpf.Services.Downloader;
+using YoutubeDownloaderWpf.Services.Logging;
 
 namespace YoutubeDownloaderWpf
 {
@@ -24,8 +26,11 @@ namespace YoutubeDownloaderWpf
         private static ServiceProvider InitializeServices()
         {
             var serviceCollection = new ServiceCollection();
-            serviceCollection.AddSingleton<IDownloader,YoutubeDownloader>();
+            serviceCollection.AddSingleton<IDownloader, YoutubeDownloader>();
             serviceCollection.AddSingleton<MainWindow>();
+            serviceCollection.AddLogging(builder =>
+                builder.AddProvider(new FileLoggerProvider("logs.txt"))
+            );
             return serviceCollection.BuildServiceProvider();
         }
 
---
+++ YoutubeDownloaderWpf/Services/Logging/SimpleStreamLogger.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YoutubeDownloaderWpf.Services.Logging
+{
+    public class SimpleStreamLogger(string loggerName, Stream outStream, LogLevel minLogLevel) : ILogger, IDisposable
+    {
+        private readonly string _loggerName = loggerName;
+        private readonly LogLevel _minLogLevel = minLogLevel;
+        private readonly StreamWriter _writer = new(outStream);
+
+        public IDisposable? BeginScope<TState>(TState state) where TState : notnull
+        {
+            return null;
+        }
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+            _writer.Dispose();
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        => logLevel >= _minLogLevel && logLevel != LogLevel.None;
+
+        public async void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+        {
+            await _writer.WriteLineAsync($"[{_loggerName}]({logLevel}): {formatter(state, exception)}");
+        }
+    }
+}
