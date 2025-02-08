73d6f2b53a0b9d798d81b9019d2d47318e05f53b
professionalowo <martin.brandl1307@gmail.com>
2025-02-09 00:18:35 +0100

    added ScopedResource abstraction for files that should be deleted after scope exits

--- YoutubeDownloaderWpf/Services/AutoUpdater/FfmpegDownloader.cs
+++ YoutubeDownloaderWpf/Services/AutoUpdater/FfmpegDownloader.cs
@@ -10,6 +10,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using YoutubeDownloaderWpf.Services.InternalDirectory;
+using YoutubeDownloaderWpf.Util.ScopedResource;
 
 namespace YoutubeDownloaderWpf.Services.AutoUpdater
 {
@@ -17,53 +18,45 @@ namespace YoutubeDownloaderWpf.Services.AutoUpdater
     {
         public async ValueTask<bool> DownloadFfmpeg()
         {
-            string? zipSource = null;
-            string? sourceUnzipped = null;
+
             if (DoesFfmpegExist(config)) return true;
-            try
+
+
+
+            Directory.CreateDirectory(config.FfmpegFolder.FullPath);
+
+            var res = MessageBox.Show("YoutubeDowloader wants to download ffmpeg.\nContinue?", "Downlaod Ffmpeg", MessageBoxButton.OKCancel, MessageBoxImage.Question);
+
+            if (res != MessageBoxResult.OK) return false;
+
+            using var response = await client.GetAsync(Config.Source);
+            using var readStream = await response.Content.ReadAsStreamAsync();
+
+            using ScopedResource.File zipSourceFile = new(config.FfmpegFolder.SaveFileName("source.zip"));
+            string zipSource = zipSourceFile.FullPath;
+            using ScopedResource.Directory sourceUnzippedDir = new(config.FfmpegFolder.SaveFileName(Path.GetFileNameWithoutExtension(zipSource)));
+            string sourceUnzipped = sourceUnzippedDir.FullPath;
+
+            using (var fileStream = new FileStream(zipSource, FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite | FileShare.Delete))
             {
-
-
-                Directory.CreateDirectory(config.FfmpegFolder.FullPath);
-
-                var res = MessageBox.Show("YoutubeDowloader wants to download ffmpeg.\nContinue?", "Downlaod Ffmpeg", MessageBoxButton.OKCancel, MessageBoxImage.Question);
-
-                if (res != MessageBoxResult.OK) return false;
-
-                using var response = await client.GetAsync(Config.Source);
-                using var readStream = await response.Content.ReadAsStreamAsync();
-
-                zipSource = config.FfmpegFolder.SaveFileName("source.zip");
-                sourceUnzipped = config.FfmpegFolder.SaveFileName(Path.GetFileNameWithoutExtension(zipSource));
-
-                using (var fileStream = new FileStream(zipSource, FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite | FileShare.Delete))
-                {
-                    readStream.CopyTo(fileStream);
-                }
-
-
-                ZipFile.ExtractToDirectory(zipSource, sourceUnzipped);
-                string[] executables = Directory.GetFiles(sourceUnzipped, "*.exe", SearchOption.AllDirectories);
-
-
-
-                string ffmpegExe = AppendExe(config.FfmpegExeName);
-                string ffprobeExe = AppendExe(config.FfprobeExeName);
-
-                string ffmpegPathSource = executables.Where(path => path.EndsWith(ffmpegExe)).First();
-                string ffprobePathSource = executables.Where(path => path.EndsWith(ffprobeExe)).First();
-
-                File.Move(ffmpegPathSource, config.FfmpegFolder.SaveFileName(ffmpegExe));
-                File.Move(ffprobePathSource, config.FfmpegFolder.SaveFileName(ffprobeExe));
-
+                readStream.CopyTo(fileStream);
             }
-            finally
-            {
-                if (zipSource != null && File.Exists(zipSource))
-                    File.Delete(zipSource);
-                if (sourceUnzipped != null && Directory.Exists(sourceUnzipped))
-                    Directory.Delete(sourceUnzipped, true);
-            }
+
+
+            ZipFile.ExtractToDirectory(zipSource, sourceUnzipped);
+            string[] executables = Directory.GetFiles(sourceUnzipped, "*.exe", SearchOption.AllDirectories);
+
+
+
+            string ffmpegExe = AppendExe(config.FfmpegExeName);
+            string ffprobeExe = AppendExe(config.FfprobeExeName);
+
+            string ffmpegPathSource = executables.Where(path => path.EndsWith(ffmpegExe)).First();
+            string ffprobePathSource = executables.Where(path => path.EndsWith(ffprobeExe)).First();
+
+            File.Move(ffmpegPathSource, config.FfmpegFolder.SaveFileName(ffmpegExe));
+            File.Move(ffprobePathSource, config.FfmpegFolder.SaveFileName(ffprobeExe));
+
             return true;
         }
         private static string AppendExe(string path) => path + ".exe";
---
+++ YoutubeDownloaderWpf/Util/ScopedResource.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace YoutubeDownloaderWpf.Util.ScopedResource
+{
+    public abstract class ScopedResource : IDisposable
+    {
+        protected readonly string _path;
+        public string FullPath => _path;
+        private bool disposedValue;
+        public ScopedResource(string path)
+        {
+            _path = path;
+        }
+
+
+
+        protected abstract void CleanResource();
+
+        protected void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing && Path.Exists(FullPath))
+                {
+                    // TODO: dispose managed state (managed objects)
+                    CleanResource();
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        // ~ScopedResource()
+        // {
+        //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        //     Dispose(disposing: false);
+        // }
+
+        void IDisposable.Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+        public class File(string path) : ScopedResource(path)
+        {
+            protected override void CleanResource()
+            => System.IO.File.Delete(path);
+        }
+
+        public class Directory(string path) : ScopedResource(path)
+        {
+            protected override void CleanResource()
+            => System.IO.Directory.Delete(path, true);
+        }
+    }
+}
