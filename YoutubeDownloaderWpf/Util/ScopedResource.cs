3672498e86e70d8c43428cf5ee15494cc1545a8d
professionalowo <martin.brandl1307@gmail.com>
2025-02-09 12:08:08 +0100

    removed comments

--- YoutubeDownloaderWpf/Util/ScopedResource.cs
+++ YoutubeDownloaderWpf/Util/ScopedResource.cs
@@ -30,19 +30,9 @@ namespace YoutubeDownloaderWpf.Util.ScopedResource
                     // TODO: dispose managed state (managed objects)
                     CleanResource();
                 }
-
-                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
-                // TODO: set large fields to null
                 disposedValue = true;
             }
         }
-
-        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
-        // ~ScopedResource()
-        // {
-        //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-        //     Dispose(disposing: false);
-        // }
 
         void IDisposable.Dispose()
         {
