aa13d62c8d74868070b75efc55418b7ad0a48573
professionalowo <martin.brandl1307@gmail.com>
2025-02-22 14:05:12 +0100

    removed fields

--- YoutubeDownloaderWpf/Services/AutoUpdater/Updater.cs
+++ YoutubeDownloaderWpf/Services/AutoUpdater/Updater.cs
@@ -6,9 +6,6 @@ namespace YoutubeDownloaderWpf.Services.AutoUpdater;
 
 public class Updater(ILogger<Updater> logger, HttpClient client)
 {
-    private readonly ILogger<Updater> _logger = logger;
-    private readonly HttpClient _httpClient = client;
-
     public ValueTask<bool> IsNewVersionAvailable()
     {
         return ValueTask.FromResult(false);
