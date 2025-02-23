8e5859ab37780337a940a5cf2c30030973e2c85b
professionalowo <martin.brandl1307@gmail.com>
2025-02-23 14:52:43 +0100

    fixed illegal characters in playlist title

--- YoutubeDownloaderWpf/Services/Downloader/Download/PlaylistDownload.cs
+++ YoutubeDownloaderWpf/Services/Downloader/Download/PlaylistDownload.cs
@@ -10,6 +10,7 @@ using System.Threading.Tasks;
 using YoutubeDownloaderWpf.Controls;
 using YoutubeDownloaderWpf.Data;
 using YoutubeDownloaderWpf.Services.InternalDirectory;
+using YoutubeDownloaderWpf.Util.Extensions;
 using YoutubeExplode;
 using YoutubeExplode.Playlists;
 using YoutubeExplode.Videos;
@@ -24,7 +25,7 @@ public class PlaylistDownload(
     public async IAsyncEnumerator<VideoDownload> GetAsyncEnumerator(CancellationToken cancellationToken = default)
     {
         var playlist = await client.Playlists.GetAsync(url, cancellationToken);
-        var dir = await downloads.CreateSubDirectoryAsync(playlist.Title);
+        var dir = await downloads.CreateSubDirectoryAsync(playlist.Title.ReplaceIllegalCharacters());
         await foreach (var video in client.Playlists.GetVideosAsync(url, cancellationToken))
         {
             yield return new VideoDownload(client, video.Url, dir.Name);
