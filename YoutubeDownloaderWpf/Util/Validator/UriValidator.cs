2babbf5929f839f33bb3e40f20e2e16fd8a1779d
professionalowo <martin.brandl1307@gmail.com>
2025-02-25 15:17:44 +0100

    added validation for url

---
+++ YoutubeDownloaderWpf/Util/Validator/UriValidator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace YoutubeDownloaderWpf.Util.Validator;
+public class UriValidator : ValidationRule
+{
+    public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+    {
+        if (value is string url)
+        {
+            if (string.IsNullOrEmpty(url))
+            {
+                return new(false, "url can't be empty");
+            }
+            if (!IsUriValid(url))
+            {
+                return new(false, "url is malformed");
+            }
+            return ValidationResult.ValidResult;
+        }
+        return new(false, "unreachable");
+    }
+
+    private static bool IsUriValid(string uri) => Uri.TryCreate(uri, UriKind.Absolute, out var _);
+}
