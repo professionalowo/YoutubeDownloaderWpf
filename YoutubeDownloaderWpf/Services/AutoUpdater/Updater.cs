16d29a6d012895e36253b3bb31f28570548f0e5d
professionalowo <martin.brandl1307@gmail.com>
2025-02-25 14:57:09 +0100

    refactored handlöing of invalid versions

--- YoutubeDownloaderWpf/Services/AutoUpdater/Updater.cs
+++ YoutubeDownloaderWpf/Services/AutoUpdater/Updater.cs
@@ -13,22 +13,11 @@ public class Updater(ILogger<Updater> logger, HttpClient client, Updater.Version
 {
     [StringSyntax(StringSyntaxAttribute.Uri)]
     public const string LatestUrl = "https://github.com/professionalowo/YoutubeDownloaderWpf/releases/latest";
-    public async ValueTask<bool> IsNewVersionAvailable(CancellationToken token = default)
+    public async Task<bool> IsNewVersionAvailable(CancellationToken token = default)
     {
         try
         {
-            HttpResponseMessage response = await client.GetAsync(LatestUrl, token);
-            if (response.StatusCode != System.Net.HttpStatusCode.OK)
-            {
-                return false;
-            }
-            string? location = response.RequestMessage?.RequestUri?.ToString();
-            if (location is null)
-            {
-                return false;
-            }
-            string tag = location.Split("/").Last().Trim().TrimStart('v');
-            Version? githubVersion = Version.FromTag(tag);
+            Version githubVersion = await GetNewestVersion(token);
             return currentVersion.CompareTo(githubVersion) < 0;
         }
         catch (Exception ex)
@@ -43,8 +32,21 @@ public class Updater(ILogger<Updater> logger, HttpClient client, Updater.Version
         return Task.CompletedTask;
     }
 
+    private async Task<Version> GetNewestVersion(CancellationToken token = default)
+    {
+        HttpResponseMessage response = await client.GetAsync(LatestUrl, token);
+        if (response.StatusCode != System.Net.HttpStatusCode.OK)
+        {
+            throw new HttpRequestException("Request was not successfull");
+        }
+        string location = (response.RequestMessage?.RequestUri?.ToString()) ?? throw new HttpRequestException("Could not get newest tag");
+        string tag = location.Split("/").Last().Trim().TrimStart('v');
+        return Version.FromTag(tag);
+    }
+
     public record Version(uint Major, uint Minor, uint Patch) : IComparable<Version>
     {
+        public static Version NullVersion => new(0, 0, 0);
         public int CompareTo(Version? other)
         => other switch
         {
@@ -55,13 +57,13 @@ public class Updater(ILogger<Updater> logger, HttpClient client, Updater.Version
             _ => 0
         };
 
-        public static Version? FromTag(string tag)
+        public static Version FromTag(string tag)
         {
             if (tag.Split('.').Select(uint.Parse).Take(3).ToArray() is [uint major, uint minor, uint patch])
             {
                 return new Version(major, minor, patch);
             }
-            return null;
+            throw new ArgumentException("Tag did not conform to the pattern {major}.{minor}.{patch}");
         }
     }
 }
