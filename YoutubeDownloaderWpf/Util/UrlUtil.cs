e0886bbfcb63f7913d2d494fee5d821bea5a2655
professionalowo <martin.brandl1307@gmail.com>
2025-03-02 12:03:00 +0100

    added a bit of comments

--- YoutubeDownloaderWpf/Util/UrlUtil.cs
+++ YoutubeDownloaderWpf/Util/UrlUtil.cs
@@ -12,6 +12,12 @@ public static class UrlUtil
     private static string Combine(string first, string second)
         => $"{first.Trim('/')}/{second.Trim('/')}";
 
+    /// <summary>
+    /// Combines a variable amount of url segments
+    /// </summary>
+    /// <param name="first">The first segment</param>
+    /// <param name="rest">A variable amount of other segments in 0...n</param>
+    /// <returns>A combination of the segments separated by '/'</returns>
     public static string Combine(string first, params ReadOnlySpan<string> rest) => rest switch
     {
         [] => first,
