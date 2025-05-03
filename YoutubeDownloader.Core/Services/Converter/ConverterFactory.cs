2306bdc62705c7f773d8ba013f4bae4f05644590
professionalowo <martin.brandl1307@gmail.com>
2025-05-03 18:16:00 +0200

    use actual type

--- YoutubeDownloader.Core/Services/Converter/ConverterFactory.cs
+++ YoutubeDownloader.Core/Services/Converter/ConverterFactory.cs
@@ -2,10 +2,15 @@
 
 namespace YoutubeDownloader.Core.Services.Converter;
 
-public class ConverterFactory<TContext>(FfmpegDownloader.Config config) where TContext : IConverter<TContext>.IConverterContext
+public class ConverterFactory<TContext>(FfmpegDownloader.Config config)
+    where TContext : IConverter<TContext>.IConverterContext
 {
-    private readonly Lazy<IConverter<TContext>> _mp3Converter = new(() => new Mp3Converter<TContext>(config));
-    private readonly Lazy<IConverter<TContext>> _noopConverter = new(() => new WriteThroughConverter<TContext>(".mp4"));
+    private readonly Lazy<Mp3Converter<TContext>> _mp3Converter =
+        new(() => new Mp3Converter<TContext>(config));
+
+    private readonly Lazy<WriteThroughConverter<TContext>> _noopConverter =
+        new(() => new WriteThroughConverter<TContext>(".mp4"));
+
     public IConverter<TContext> GetConverter(bool forceMp3) => forceMp3 switch
     {
         true => _mp3Converter.Value,
