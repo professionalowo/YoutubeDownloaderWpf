bf903348dd5dc07be0f7f22ae120175444e8925a
professionalowo <martin.brandl1307@gmail.com>
2025-05-13 17:26:40 +0200

    reformatted Deconstruct method

--- YoutubeDownloader.Core/Util/Extensions/ChannelExtensions.cs
+++ YoutubeDownloader.Core/Util/Extensions/ChannelExtensions.cs
@@ -4,9 +4,6 @@ namespace YoutubeDownloader.Core.Util.Extensions;
 
 public static class ChannelExtensions
 {
-    public static void Deconstruct<T>(this Channel<T> channel, out ChannelReader<T> reader, out ChannelWriter<T> writer)
-    {
-        reader = channel.Reader;
-        writer = channel.Writer;
-    }
+    public static void Deconstruct<T>(this Channel<T> c, out ChannelReader<T> reader, out ChannelWriter<T> writer)
+        => (reader, writer) = (c.Reader, c.Writer);
 }
