85fd1ef24d3856eae24126af361f32a29394aa59
professionalowo <martin.brandl1307@gmail.com>
2025-05-21 17:27:50 +0200

    reformatted MainPage

--- YoutubeDownloader.Maui/MainPage.xaml.cs
+++ YoutubeDownloader.Maui/MainPage.xaml.cs
@@ -4,12 +4,15 @@ using YoutubeDownloader.Core.Services.Converter;
 using YoutubeDownloader.Core.Services.InternalDirectory;
 
 namespace YoutubeDownloader.Maui;
+
 using Microsoft.Maui.Controls;
+
 public partial class MainPage : ContentPage
 {
     private readonly Services.YoutubeDownloader _downloader;
     private readonly IDirectory _downloads;
-    public MainPage(Services.YoutubeDownloader downloader,IDirectory downloads)
+
+    public MainPage(Services.YoutubeDownloader downloader, IDirectory downloads)
     {
         _downloader = downloader;
         _downloads = downloads;
