7a66661f5a295f268fa738e5117c463453511f17
professionalowo <martin.brandl1307@gmail.com>
2026-02-17 15:30:37 +0100

    refactor FileLoggerProvider to improve log file handling and resource management

--- YoutubeDownloader.Core/Services/Logging/FileLoggerProvider.cs
+++ YoutubeDownloader.Core/Services/Logging/FileLoggerProvider.cs
@@ -4,21 +4,33 @@ namespace YoutubeDownloader.Core.Services.Logging;
 
 public sealed class FileLoggerProvider(string fileName) : ILoggerProvider
 {
+    private Stream? _stream;
+
     private static FileStream CreateOrOpenLogFile(string fileName)
     {
         var cwd = Directory.GetCurrentDirectory() ?? throw new IOException("Couldn't get CWD");
         var fullPath = Path.GetFullPath(Path.Combine(cwd, "logs", fileName)) ??
-                          throw new IOException($"Couldn't get path to log file {fileName}");
+                       throw new IOException($"Couldn't get path to log file {fileName}");
         var dirName = Path.GetDirectoryName(fullPath) ??
-                         throw new IOException($"Couldn't get path to log file folder");
+                      throw new IOException("Couldn't get path to log file folder");
         Directory.CreateDirectory(dirName);
-        return File.Open(fullPath, FileMode.OpenOrCreate, FileAccess.Write, FileShare.Write);
+        return File.Open(
+            fullPath,
+            FileMode.Append,
+            FileAccess.Write,
+            FileShare.ReadWrite
+        );
     }
 
     public ILogger CreateLogger(string categoryName)
-        => new SimpleStreamLogger(categoryName, new BufferedStream(CreateOrOpenLogFile(fileName)));
+    {
+        _stream = new BufferedStream(CreateOrOpenLogFile(fileName));
+        return new SimpleStreamLogger(categoryName, _stream);
+    }
+
 
     public void Dispose()
     {
+        _stream?.Dispose();
     }
 }
