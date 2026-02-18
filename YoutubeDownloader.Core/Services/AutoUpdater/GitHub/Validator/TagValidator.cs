8d8e01ee213c878f18bea06a5e1a877fca0c6771
professionalowo <martin.brandl1307@gmail.com>
2026-02-18 16:44:48 +0100

    refactor TagValidator to simplify regex generation

--- YoutubeDownloader.Core/Services/AutoUpdater/GitHub/Validator/TagValidator.cs
+++ YoutubeDownloader.Core/Services/AutoUpdater/GitHub/Validator/TagValidator.cs
@@ -4,7 +4,7 @@ namespace YoutubeDownloader.Core.Services.AutoUpdater.GitHub.Validator;
 
 public static partial class TagValidator
 {
-    [GeneratedRegex(@"^.*/v\d+\.\d+\.\d+$", RegexOptions.NonBacktracking)]
+    [GeneratedRegex(@"^.*/v\d+\.\d+\.\d+$")]
     private static partial Regex TagRegex();
 
     public static bool IsValid(string toTest)
