da95ed16500827d1c6360086dac65523fc03fd05
professionalowo <martin.brandl1307@gmail.com>
2026-02-19 10:43:41 +0100

    refactor ScopedResource and remove unused utility; update PlatformSpecificServiceExtensions to use FfmpegConfig

--- YoutubeDownloader.Maui/Platforms/MacCatalyst/PlatformSpecificServiceExtensions.cs
+++ YoutubeDownloader.Maui/Platforms/MacCatalyst/PlatformSpecificServiceExtensions.cs
@@ -1,15 +1,17 @@
 using Foundation;
 using YoutubeDownloader.Core.Services.AutoUpdater.Ffmpeg;
 using YoutubeDownloader.Core.Services.InternalDirectory;
+
 namespace YoutubeDownloader.Maui.Util;
 
 public static partial class PlatformSpecificServiceExtensions
 {
-    public static partial IServiceCollection AddFfmpeg(this IServiceCollection serviceCollection,IDirectory directory)
+    public static partial IServiceCollection AddFfmpeg(this IServiceCollection serviceCollection, IDirectory directory)
     {
-        var config = new FfmpegDownloader.Config(directory,Path.Combine(NSBundle.MainBundle.ResourcePath, "ffmpeg"));
+        var config = new FfmpegConfig(directory, FfmpegConfig.SourceUri,
+            Path.Combine(NSBundle.MainBundle.ResourcePath, "ffmpeg"));
         serviceCollection.AddSingleton(config);
-        
+
         return serviceCollection;
     }
 }
