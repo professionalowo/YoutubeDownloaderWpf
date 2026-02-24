7e4e6743830c8c6c63a0778d5027157f3de1aa67
professionalowo <martin.brandl1307@gmail.com>
2026-02-24 20:27:42 +0100

    refactor InverseBooleanConverter to use nullable reference types

--- YoutubeDownloader.Wpf/Controls/InverseBooleanConverter.cs
+++ YoutubeDownloader.Wpf/Controls/InverseBooleanConverter.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Globalization;
 using System.Windows.Data;
 
@@ -6,13 +7,13 @@ namespace YoutubeDownloader.Wpf.Controls;
 [ValueConversion(typeof(bool), typeof(bool))]
 public sealed class InverseBooleanConverter : IValueConverter
 {
-    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
         if (value is bool b) return !b;
         return false;
     }
 
-    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
         if (value is bool b) return !b;
         return false;
