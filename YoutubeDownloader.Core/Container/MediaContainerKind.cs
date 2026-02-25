e2350dbe346269cdb94b208cf191ff8d9aaec28f
professionalowo <martin.brandl1307@gmail.com>
2026-02-25 20:40:36 +0100

    Refactor: update Wav and Opus implementations in MediaContainerKind to define file extensions and codecs

--- YoutubeDownloader.Core/Container/MediaContainerKind.cs
+++ YoutubeDownloader.Core/Container/MediaContainerKind.cs
@@ -6,7 +6,6 @@ internal sealed record Mp3 : IMediaContainer
 
     public IMediaContainer.FileExtension Extension => new("mp3");
     public IMediaContainer.Codec FfmpegCodec => new("libmp3lame");
-
     public IMediaContainer.Codec.Flags FfmpegCodecFlags =>
     [
         "-q:a", "2",
@@ -17,18 +16,18 @@ internal sealed record Mp3 : IMediaContainer
 internal sealed record Wav : IMediaContainer
 {
     public override string ToString() => "Wav Audio";
+    
     public IMediaContainer.FileExtension Extension => new("wav");
     public IMediaContainer.Codec FfmpegCodec => new("pcm_s16le");
-
     public IMediaContainer.Codec.Flags FfmpegCodecFlags => ["-ar", "48000", "-ac", "2"];
 }
 
 internal sealed record Opus : IMediaContainer
 {
     public override string ToString() => "Opus Audio";
+    
     public IMediaContainer.FileExtension Extension => new("opus");
     public IMediaContainer.Codec FfmpegCodec => new("libopus");
-
     public IMediaContainer.Codec.Flags FfmpegCodecFlags => ["-b:a", "128k", "-vbr", "on", "-compression_level", "10"];
 }
 
