09ed5caba28cfa86ae608be21922e3178c506757
professionalowo <martin.brandl1307@gmail.com>
2026-03-01 12:44:41 +0100

    Refactor: add AsyncEnumerableExtensions for improved async handling of single items

---
+++ YoutubeDownloader.Core/Extensions/AsyncEnumerableExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace YoutubeDownloader.Core.Extensions;
+
+public static class AsyncEnumerableExtensions
+{
+    extension<T>(AsyncEnumerable)
+    {
+        public static async IAsyncEnumerable<T> FromSingle(T item,
+            [EnumeratorCancellation] CancellationToken token = default)
+        {
+            token.ThrowIfCancellationRequested();
+            yield return item;
+            await Task.CompletedTask
+                .ConfigureAwait(false);
+        }
+    }
+}
--- YoutubeDownloader.Core/Services/Downloader/Download/DownloadFactory.cs
+++ YoutubeDownloader.Core/Services/Downloader/Download/DownloadFactory.cs
@@ -8,27 +8,37 @@ namespace YoutubeDownloader.Core.Services.Downloader.Download;
 
 public sealed class DownloadFactory(YoutubeClient client, IDirectory downloads)
 {
-    public async IAsyncEnumerable<VideoDownload> Get([StringSyntax(StringSyntaxAttribute.Uri)] string url,
+    private static bool IsVideo([StringSyntax(StringSyntaxAttribute.Uri)] ReadOnlySpan<char> url)
+    {
+        var lastSlashIdx = url.LastIndexOf('/');
+
+        if (lastSlashIdx == -1 || lastSlashIdx == url.Length - 1)
+            throw new ArgumentException("Invalid url", nameof(url));
+
+        var charAfterLastSlash = url[lastSlashIdx + 1];
+        return charAfterLastSlash == 'w';
+    }
+
+
+    public IAsyncEnumerable<VideoDownload> Get([StringSyntax(StringSyntaxAttribute.Uri)] string url,
+        CancellationToken token = default)
+        => IsVideo(url)
+            ? AsyncEnumerable.FromSingle(new VideoDownload(client, url), token)
+            : GetPlaylist(url, token);
+
+
+    private async IAsyncEnumerable<VideoDownload> GetPlaylist([StringSyntax(StringSyntaxAttribute.Uri)] string url,
         [EnumeratorCancellation] CancellationToken token = default)
     {
-        token.ThrowIfCancellationRequested();
-        var last = url.Split('/').Last().First();
-        if (last == 'w')
+        var playlist = await client.Playlists.GetAsync(url, token)
+            .ConfigureAwait(false);
+        var dir = await downloads.CreateSubDirectoryAsync(playlist.Title.ReplaceIllegalCharacters())
+            .ConfigureAwait(false);
+        var enumerable = client.Playlists.GetVideosAsync(url, token)
+            .ConfigureAwait(false);
+        await foreach (var video in enumerable)
         {
-            yield return new VideoDownload(client, url);
-        }
-        else
-        {
-            var playlist = await client.Playlists.GetAsync(url, token)
-                .ConfigureAwait(false);
-            var dir = await downloads.CreateSubDirectoryAsync(playlist.Title.ReplaceIllegalCharacters())
-                .ConfigureAwait(false);
-            var enumerable = client.Playlists.GetVideosAsync(url, token)
-                .ConfigureAwait(false);
-            await foreach (var video in enumerable)
-            {
-                yield return new VideoDownload(client, video.Url, dir.Name);
-            }
+            yield return new VideoDownload(client, video.Url, dir.Name);
         }
     }
 }
