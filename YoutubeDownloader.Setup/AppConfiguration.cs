446fb94101c86138e796f785e317b7cae89ccc2c
professionalowo <martin.brandl1307@gmail.com>
2026-03-22 17:09:39 +0100

    Refactor: add AppConfiguration class and update service registration to use configuration options

---
+++ YoutubeDownloader.Setup/AppConfiguration.cs
@@ -0,0 +1,6 @@
+﻿namespace YoutubeDownloader.Setup;
+
+public class AppConfiguration
+{
+    public string DownloadPath { get; set; } = "Downloads";
+}
--- YoutubeDownloader.Setup/ServiceCollectionExtensions.cs
+++ YoutubeDownloader.Setup/ServiceCollectionExtensions.cs
@@ -58,7 +58,7 @@ public static class ServiceCollectionExtensions
                 .AddJsonFile("settings.json", true, true)
                 .Build();
 
-            return serviceCollection.AddSingleton(config);
+            return serviceCollection.Configure<AppConfiguration>(config);
         }
     }
 }
